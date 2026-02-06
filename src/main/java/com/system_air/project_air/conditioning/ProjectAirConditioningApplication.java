f360b2264f9c349473242da8f29243a75f809e42
LucasSalees <saleslucaas01@gmail.com>
2026-02-06 16:29:22 -0300

    Fix: Forçando varredura completa de pacotes com ComponentScan

--- src/main/java/com/system_air/project_air/conditioning/ProjectAirConditioningApplication.java
+++ src/main/java/com/system_air/project_air/conditioning/ProjectAirConditioningApplication.java
@@ -2,10 +2,14 @@ package com.system_air.project_air.conditioning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.system_air"}) // Isso resolve o conflito dos nomes dos pacotes
+@ComponentScan(basePackages = "com.system_air")
+@EntityScan(basePackages = "com.system_air")
+@EnableJpaRepositories(basePackages = "com.system_air")
 public class ProjectAirConditioningApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectAirConditioningApplication.class, args);
